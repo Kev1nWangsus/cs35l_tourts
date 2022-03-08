2630f42f85a68e58af9ec880112e479446d55dbb
崔馨月 <fionac411@g.ucla.edu>
2022-03-08 15:27:21 -0800

    Implement sorting function based on different matching criterion

--- frontend/src/common/functions/compare.js
+++ frontend/src/common/functions/compare.js
@@ -1,96 +1,154 @@
 import {region, in_region} from './function';
 
-function sortByRatingAsc(user, apps) {
+function ratingMatch(user, app) {
 
-    function compare(a, b) {
-        if (Math.abs(a.rating - user.rating) < Math.abs(b.rating - user.rating)) {
-            return -1;
-        }
-        else if (Math.abs(a.rating - user.rating) > Math.abs(b.rating - user.rating)) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    }
 
-    return (apps.sort(compare));
+    return (Math.abs(user.rating - app.rating));
 
 }
 
-
-function sortByRatingDesc(user, apps) {
-
-    function compare(a, b) {
-        if (Math.abs(a.rating - user.rating) < Math.abs(b.rating - user.rating)) {
-            return 1;
-        }
-        else if (Math.abs(a.rating - user.rating) > Math.abs(b.rating - user.rating)) {
-            return -1;
-        }
-        else {
-            return 0;
-        }
-    }
-
-    return (apps.sort(compare));
-
-}
-
-
-function sortByRegiongAsc(user, apps) {
-
+// return 1 if user and app are in the same region,
+// otherwise return 0
+function regionMatch (user, app) {
     user_region_large = region(user.region);
-
-    var in_reg = [];
-    var not_in_reg = [];
-
-    for (let i = 0; i < apps.length; i++) {
-        if (in_region(apps[i].region, user_region_large)) {
-            in_reg.push(apps[i]);
-        }
-        else {
-            not_in_reg.push(apps[i]);
-        }
+    if (in_region(app.region, user_region_large)) {
+        return 1;
     }
-
-    function compare(a, b) {
-        
-        if (a.region < b.region) {
-            return -1;
-        }
-        else if (a.region > b.region) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    }
-
-    sorted_apps = in_reg.sort(compare).concat(not_in_reg.sort(compare));
-    return sorted_apps;
-
+    return 0;
 }
 
-function sortByRegiongDesc(user, apps) {
+function timeOverlap(user_start, user_end, app) {
+    user_start_in_min = 60 * Number(user_start.substring(0, 2)) + Number(user_start.substring(3));
+    user_end_in_min = 60 * Number(user_end.substring(0, 2)) + Number(user_end.substring(3));
 
-    user_region_large = region(user.region);
+    app_start_in_min = 60 * Number(app.start.substring(0, 2)) + Number(app.start.substring(3));
+    app_end_in_min = 60 * Number(app.end.substring(0, 2)) + Number(app.end.substring(3));
 
-    var in_reg = [];
-    var not_in_reg = [];
-
-    for (let i = 0; i < apps.length; i++) {
-        if (in_region(apps[i].region, user_region_large)) {
-            in_reg.push(apps[i]);
-        }
-        else {
-            not_in_reg.push(apps[i]);
-        }
+    // no overlap
+    if (user_start_in_min >= app_end_in_min || app_start_in_min >= user_end_in_min) {
+        return 0;
     }
 
-    result_apps = not_in_reg.concat(in_reg);
+    user_timeframe = user_end_in_min - user_start_in_min;
 
-    return result_apps;
+    if (user_start_in_min <= app_start_in_min && app_end_in_min <= user_end_in_min) {
+        overlap = app_end_in_min - app_start_in_min;
+    }
 
+    else if (app_start_in_min <= user_start_in_min && user_end_in_min <= app_end_in_min) {
+        overlap = user_timeframe;
+    }
+
+    else if (user_start_in_min <= app_start_in_min) {
+        overlap = user_end_in_min - app_start_in_min;
+    }
+
+    // app_start_in_min <= user_start_in_min
+    else {
+        overlap = app_end_in_min - user_start_in_min;
+    }
+
+    return (overlap / user_timeframe);
 }
 
+// default sorting criteria 0: time > rating > region
+// sorting criteria 1: rating > time > region
+// sorting criteria 2: region > time > rating
+function sorting (user, user_start, user_end, apps, criteria = 0) {
+    if (criteria == 0) {
+        function compare(a, b) {
+            if (timeOverlap(user_start, user_end, a) > timeOverlap(user_start, user_end, b)) {
+                return -1;
+            }
+            else if (timeOverlap(user_start, user_end, a) < timeOverlap(user_start, user_end, b)) {
+                return 1;
+            }
+            else {
+                if (ratingMatch(user, a) < ratingMatch(user, b)) {
+                    return -1;
+                }
+                else if (ratingMatch(user, a) > ratingMatch(user, b)) {
+                    return 1;
+                }
+                else {
+                    if (regionMatch(user, a) > regionMatch(user, b)) {
+                        return -1;
+                    }
+                    else if (regionMatch(user, a) < regionMatch(user, b)) {
+                        return 1;
+                    }
+                    else {
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        return (apps.sort(compare));
+    }
+
+    else if (criteria == 1) {
+        function compare(a, b) {
+            if (ratingMatch(user, a) < ratingMatch(user, b)) {
+                return -1;
+            }
+            else if (ratingMatch(user, a) > ratingMatch(user, b)) {
+                return 1;
+            }
+            else {
+                if (timeOverlap(user_start, user_end, a) > timeOverlap(user_start, user_end, b)) {
+                    return -1;
+                }
+                else if (timeOverlap(user_start, user_end, a) < timeOverlap(user_start, user_end, b)) {
+                    return 1;
+                }
+                else {
+                    if (regionMatch(user, a) > regionMatch(user, b)) {
+                        return -1;
+                    }
+                    else if (regionMatch(user, a) < regionMatch(user, b)) {
+                        return 1;
+                    }
+                    else {
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        return (apps.sort(compare));
+    }
+
+    else {
+        function compare(a, b) {
+            if (regionMatch(user, a) > regionMatch(user, b)) {
+                return -1;
+            }
+            else if (regionMatch(user, a) < regionMatch(user, b)) {
+                return 1;
+            }
+            else {
+                if (timeOverlap(user_start, user_end, a) > timeOverlap(user_start, user_end, b)) {
+                    return -1;
+                }
+                else if (timeOverlap(user_start, user_end, a) < timeOverlap(user_start, user_end, b)) {
+                    return 1;
+                }
+                else {
+                    if (ratingMatch(user, a) < ratingMatch(user, b)) {
+                        return -1;
+                    }
+                    else if (ratingMatch(user, a) > ratingMatch(user, b)) {
+                        return 1;
+                    }
+                    else {
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        return (apps.sort(compare));
+    }
+
+}
