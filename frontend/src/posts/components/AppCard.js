3868dccb8751024ae7eaabbf6456f71f9ab593f8
Ethan_Jiang <1766569133@qq.com>
2022-03-09 03:20:50 -0800

    info page after clicking more

--- frontend/src/posts/components/AppCard.js
+++ frontend/src/posts/components/AppCard.js
@@ -1,27 +1,63 @@
 import {
   Button,
   Card,
+  Grid,
+  Backdrop,
   CardContent,
+  DialogContent,
+  CircularProgress,
   CardMedia,
   Stack,
   Typography,
 } from '@mui/material';
 import { format } from 'date-fns';
-import React from 'react';
 import { useHttpClient } from '../../common/hooks/http-hook';
+import Modal from '@mui/material/Modal';
+import React, { useState, useEffect } from 'react';
+import FormContainer from '../../common/components/FormElement/FormContainer';
 
 const AppCard = (props) => {
   const { id, image, creator, title, description, date, start, end, address } =
     props.app;
-  const accept = creator != localStorage.getItem('user');
-  const usdate = date + 'T00:00:00-08:00';
-  const formatDate = format(new Date(usdate), 'PPP');
-  const { isLoading, error, sendRequest } = useHttpClient();
+  const accept = creator !== localStorage.getItem('user');
+  const formatDate = format(new Date(date), 'PPP');
+  const { isLoading, sendRequest } = useHttpClient();
+
+  const [open, setOpen] = useState(false);
+  const [appointments, setAppointments] = useState([]);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const uid = localStorage.getItem('user');
+    // const url = 'http://localhost:5000/api/appointments';
+    const url = `http://localhost:5000/api/appointments/${uid}`;
+
+    const fetchData = async () => {
+      const [err, response] = await sendRequest(url)
+        .then((response) => [null, response])
+        .catch((err) => [err, null]);
+
+      if (err) {
+        console.log('error', err);
+      } else {
+        console.log('data', response);
+        setAppointments(response.appointments);
+      }
+    };
+    
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [sendRequest]);
 
   const handleAccept = async () => {
     const url = `http://localhost:5000/api/appointments/${id}`;
     const acceptor = localStorage.getItem('user');
-    const data = { acceptorId: acceptor };
+    const data = { acceptor: acceptor };
     console.log(localStorage.getItem('user'));
     console.log(JSON.stringify(data));
     const [err, response] = await sendRequest(
@@ -103,10 +139,77 @@ const AppCard = (props) => {
             Delete
           </Button>
         )}
-        <Button variant='outlined' color='primary' sx={{ m: 1, width: 0.2 }}>
+        <Button
+          variant='outlined'
+          color='primary'
+          onClick={handleOpen}
+          sx={{ m: 1, width: 0.2 }}
+        >
           More
         </Button>
       </Stack>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+        sx={{ mt: 8 }}
+      >
+        
+        <DialogContent>
+          <FormContainer maxWidth='md'>
+            <Typography
+              variant='h3'
+              gutterBottom
+              marked='center'
+              align='center'
+              sx={{ mb: 4 }}
+            >
+              appointment details
+            </Typography>
+            <Backdrop
+              sx={{ color: '#000', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+              open={isLoading}
+            >
+              <CircularProgress color='inherit' />
+            </Backdrop>
+            <Typography align='center' variant='h4' component='div'>
+              {title}
+            </Typography> 
+            {!isLoading && appointments && ( 
+            <Grid item xs={8} sm={15} align='center' justify='center'>
+              <h4> Description: {description}</h4>
+              <h4>{`Start Time: ${start}`}</h4>
+              <h4>{`End Time: ${end}`}</h4>
+              <h4>{`End Time: ${end}`}</h4>
+              <h4>{`Appointment Date: ${formatDate}`}</h4>
+              <h4>{`Address: ${address}`}</h4>
+              <CardMedia
+                component='img'
+                height='500'
+                image={`http://localhost:5000/${image}`}
+                alt='view'
+              />
+            </Grid>
+            )}
+            <Grid item xs={8} sm={15} align='center' justify='center'>
+              <Button
+                variant={'contained'}
+                size='large'
+                color='secondary'
+                sx={{ mt: 5, width: 0.8 }}
+                onClick={handleClose}
+              >
+                Cancel
+              </Button>
+            </Grid>
+          </FormContainer>
+        </DialogContent>
+      </Modal>
     </Card>
   );
 };
